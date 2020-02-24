a1cb16159711324b0f0df198c3c286d4567202a7
Sai Srivathsa Muthyala <muthyalasaisrivathsa@gmail.com>
2020-02-24 20:22:03 +0530

    Assignment 2

---
+++ Assignment2.java
@@ -0,0 +1,27 @@
+package Assignment2;
+
+import java.util.ArrayList;
+
+public class Assignment2 {
+	
+	public boolean allLettersAvailableOrNot(String text) {
+		StringBuilder sb = new StringBuilder(text.toLowerCase());
+		ArrayList<Character> alphabets = new ArrayList<Character>();
+		for(int i=0;i<text.length();i++) {
+			if(Character.isLetter(sb.charAt(i)) && !alphabets.contains(sb.charAt(i))) {
+				alphabets.add(sb.charAt(i));
+			}	
+			if(alphabets.size() == 26)
+				return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assignment2 obj = new Assignment2();
+		System.out.println("Checking whether the text 'qwertyuiopasdfghjklzxcvbnm' has every alphabet in it: " + obj.allLettersAvailableOrNot("qwertyuiopasdfghjklzxcvbnm"));
+		System.out.println("Checking whether the text 'qwertyuiop' has every alphabet in it: " + obj.allLettersAvailableOrNot("qwertyuiop"));
+		System.out.println("Checking whether the text 'qwertyuiopasdfghjklzxcvbN ABCDEF...' has every alphabet in it: " + obj.allLettersAvailableOrNot("qwertyuiopasdfghjklzxcvbN  ABCDEF..."));
+	}
+}
